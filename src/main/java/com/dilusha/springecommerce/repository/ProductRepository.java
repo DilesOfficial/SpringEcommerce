0fc5dc0830f6d336ce734948a62b1007514eeae6
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-18 20:16:53 +0530

    Search

--- src/main/java/com/dilusha/springecommerce/repository/ProductRepository.java
+++ src/main/java/com/dilusha/springecommerce/repository/ProductRepository.java
@@ -2,8 +2,19 @@ package com.dilusha.springecommerce.repository;
 
 import com.dilusha.springecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query("SELECT p from Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(@RequestParam String keyword);
 }
