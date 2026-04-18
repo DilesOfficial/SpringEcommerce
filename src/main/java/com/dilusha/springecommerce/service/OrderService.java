12d88a4567c21e3bba73d40d866aa1785d357aae
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-18 21:26:05 +0530

    Creating Order Controller

---
+++ src/main/java/com/dilusha/springecommerce/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.dilusha.springecommerce.service;
+
+import com.dilusha.springecommerce.model.dto.OrderRequest;
+import com.dilusha.springecommerce.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+}
